0ef8ee035521e6ee5c0e9d06086fb55ecb561298
Carlos Uresti <80980080+carlos-uresti@users.noreply.github.com>
2022-04-24 02:29:18 +0000

    working version with unaltered database

--- main.py
+++ main.py
@@ -17,19 +17,14 @@ car_database.pack()#inflates database notebook
 
 #add vehicle tab
 tab1 = Frame(car_database, width=600, height = 600)
-
 #add customer tab
 tab2 = Frame(car_database, width=600, height = 600)
-
 #customer lookup tab
 tab3 = Frame(car_database, width=600, height = 600)
-
 #vehicle lookup tab
 tab4 = Frame(car_database, width=600, height = 600)
-
 #book rental tab
 tab5 = Frame(car_database, width=600, height = 600)
-
 #return rental tab
 tab6 = Frame(car_database, width=600, height = 600)
 
@@ -42,12 +37,12 @@ tab4.pack(fill = "both", expand = 1)
 tab4.pack(fill = "both", expand = 1)
 
 
-#adds tabs to database notebook
+#adds tabs to databse notebook
 car_database.add(tab1, text = "Add Vehicle")
 car_database.add(tab2, text = "Add Customer")
 car_database.add(tab3, text = "Customer Lookup")
 car_database.add(tab4, text = "Vehicle Lookup")
-car_database.add(tab5, text = "Reserve Rental")
+car_database.add(tab5, text = "Book Rental")
 car_database.add(tab6, text = "Return Rental")
 
 #link to sqlite db file
@@ -111,11 +106,8 @@ category_label.grid(row =4, column = 0)
 #add vehicle button 
 submit_btn = Button(tab1, text ='Add Vehicle ', command = submit)
 submit_btn.grid(row = 7, column =0, columnspan = 2, pady = 10, padx = 10, ipadx = 140)
+
 ##########################################################################################
-
-
-
-
 
 
 
@@ -158,11 +150,6 @@ submit_btn = Button(tab2, text ='Add Customer ', command = submit2)
 submit_btn.grid(row = 7, column =0, columnspan = 2, pady = 10, padx = 10, ipadx = 140)
 
 ###############################################################################
-
-
-
-
-
 
 
 ####################################Cusomer LookUp##############################
@@ -214,11 +201,6 @@ phone_number_label.grid(row =1, column = 0)
 input_qry_btn = Button(tab3, text = 'List Customers', command = input_query2)
 input_qry_btn.grid(row = 8, column =0, columnspan = 2, pady = 10, padx = 10, ipadx = 140)
 ################################################################################
-
-
-
-
-
 
 
 ###################################Vehicle Lookup##############################
@@ -295,32 +277,10 @@ input_qry_btn.grid(row = 8, column =0, columnspan = 2, pady = 10, padx = 10, ipa
 
 
 
-
-
 ###########################################Book Rental###################################
 
- #submit new vehicle info
-def submit():
-	submit_conn = sqlite3.connect('rental.db')
 
-	submit_cur = submit_conn.cursor()
-
-	submit_cur.execute("INSERT INTO RENTAL VALUES (:StartDate, :EndDate, :Year, :Type, :Category) ",
-		{
-			'VehicleID': vehicle_id.get(),
-			'Description': description.get(),
-			'Year': year.get(),
-			'Type': type.get(),
-			'Category': category.get()
-		
-		})
-  ###################commit changes
-	submit_conn.commit()
-	#close the DB connection
-	submit_conn.close() 
-
-
-########################list of vehicles available in specified window
+#list query for vehicles
 def input_query():
   iq_conn = sqlite3.connect('rental.db')
   iq_cur = iq_conn.cursor()
@@ -333,15 +293,15 @@ def input_query():
   iq_conn.commit()
 	#close the DB connection
   iq_conn.close()
-  #############################results dropdown menu
+  
   vehicle = StringVar()
   vehicle.set("Select from results")
   drop3 = OptionMenu(tab5, vehicle, *output_records)
-  drop3.grid(row = 10, column =0, columnspan = 2, pady = 10, padx = 10, ipadx = 140)
+  drop3.grid(row = 10, column =1, columnspan = 2, pady = 10, padx = 10, ipadx = 140)
 
 
 
-###############################input fields
+# input fields
 customerId = Entry(tab5, width = 30)
 customerId.grid(row = 0, column = 1)
 
@@ -354,7 +314,26 @@ startDate.grid(row = 2, column = 1, padx = 20)
 endDate = Entry(tab5, width = 30)
 endDate.grid(row = 3, column = 1)
 
-###########################vehicle type drop down menu	
+#create labels
+customerId_label = Label(tab5, text = 'Customer ID: ')
+customerId_label.grid(row =0, column = 0)
+
+customer_name_label = Label(tab5, text = 'Customer Name: ')
+customer_name_label.grid(row =1, column = 0)
+
+startDate_label = Label(tab5, text = 'Start Date: ')
+startDate_label.grid(row =2, column = 0)
+
+endDate_label = Label(tab5, text = 'End Date: ')
+endDate_label.grid(row =3, column = 0)
+
+type_label = Label(tab5, text = 'Type: ')
+type_label.grid(row =4, column = 0)
+
+category_label = Label(tab5, text = 'Category: ')
+category_label.grid(row =5, column = 0)
+
+#vehicle type drop down menu	
 vehicleTypes = ["1", "2", "3", "4", "5", "6"]
 type = StringVar()
 type.set("Vehicle Type")
@@ -368,51 +347,15 @@ category.set("Vehicle Category")
 drop2 = OptionMenu(tab5, category, *vehicleCategories)
 drop2.grid(column = 1, row= 5)
 
-#############################vehicle category dropdown menu
-rentalTypes = ["1", "7"]
-rentalType = StringVar()
-rentalType.set("Rental Type")
-drop4 = OptionMenu(tab5, rentalType, *rentalTypes)
-drop4.grid(column = 1, row= 6)
-
-quantity = Entry(tab5, width = 30)
-quantity.grid(row = 7, column = 1)
-
-#############################create labels
-customerId_label = Label(tab5, text = 'Customer ID: ')
-customerId_label.grid(row =0, column = 0)
-
-customer_name_label = Label(tab5, text = 'Customer Name: ')
-customer_name_label.grid(row =1, column = 0)
-
-startDate_label = Label(tab5, text = 'Start Date*: ')
-startDate_label.grid(row =2, column = 0)
-
-endDate_label = Label(tab5, text = 'Return Date*: ')
-endDate_label.grid(row =3, column = 0)
-
-type_label = Label(tab5, text = 'Type*: ')
-type_label.grid(row =4, column = 0)
-
-category_label = Label(tab5, text = 'Category*: ')
-category_label.grid(row =5, column = 0)
-
-rentalType_label = Label(tab5, text = 'Rental Type: ')
-rentalType_label.grid(row =6, column = 0)
-
-qty_label = Label(tab5, text = 'Quantity: ')
-qty_label.grid(row =7, column = 0)
 
 #create another dropdown that displays results in a select menu rather than print them
 input_qry_btn = Button(tab5, text = 'Search available vehicles', command = input_query)
 input_qry_btn.grid(row = 8, column =0, columnspan = 2, pady = 10, padx = 10, ipadx = 140)
+
+#results dropdown menu
+
+
 #########################################################################################
-
-
-
-
-
-
 
 
 ###########################################Return Rental####################################
@@ -420,7 +363,5 @@ input_qry_btn.grid(row = 8, column =0, columnspan = 2, pady = 10, padx = 10, ipa
 
 
 #########################################################################################
-
-
 #executes tinker components
 root.mainloop()

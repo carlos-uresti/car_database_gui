dda99998df1b2872c03e4071da881d434688c54b
Shivam Patel <77644237+shivam1721@users.noreply.github.com>
2022-05-08 23:30:04 +0000

    query 5b working

--- listCustomer.py
+++ listCustomer.py
@@ -8,17 +8,20 @@ from tabs import *
 
 def listCustomer():
   #list query for customers
+  Empty = False
   def input_query2():
 
     iq_conn = sqlite3.connect('rental.db')
     iq_cur = iq_conn.cursor()
 
     #set condition to account for empty fields
-    if(customer_name2.get() == "" and phone_number2.get()== ""):
-      iq_cur.execute("SELECT * FROM CUSTOMER")
+    if(customer_name2.get() == "" and customer_id.get()== ""):
+      Empty = True
+      iq_cur.execute("SELECT CustID, Name, CASE when PaymentDate='NULL' THEN '$0.00' ELSE '$'||TotalAmount END FROM CUSTOMER NATURAL JOIN RENTAL ORDER BY TotalAmount")
     else:
-      iq_cur.execute("SELECT Name, Phone FROM CUSTOMER WHERE Name = ? OR Phone = ?",
-                  (customer_name2.get(), phone_number2.get(),))    
+      Empty = False
+      iq_cur.execute("SELECT CustID, Name FROM CUSTOMER WHERE Name = ? OR CustID = ?",
+                  (customer_name2.get(), customer_id.get(),))    
     
     #executes search query when list vehicles button is clicked 
     output_records1 = iq_cur.fetchall()
@@ -27,7 +30,10 @@ def listCustomer():
     
   #print records found
     for output_record1 in output_records1:
-      print_record += str(str(output_record1[0])+ " " + output_record1[1]+ " " +"\n")
+      if Empty:
+        print_record += str(str(output_record1[0])+ " " + str(output_record1[1])+ " " + str(output_record1[2]) +"\n")
+      else:
+        print_record += str(str(output_record1[0])+ " " + str(output_record1[1])+ "" +"\n")
   
     iq_label = Label(tab3, text = print_record)
   
@@ -43,15 +49,15 @@ def listCustomer():
   customer_name2 = Entry(tab3, width = 30)
   customer_name2.grid(row = 0, column = 1, padx = 20)
   
-  phone_number2 = Entry(tab3, width = 30)
-  phone_number2.grid(row = 1, column = 1)
+  customer_id = Entry(tab3, width = 30)
+  customer_id.grid(row = 1, column = 1)
   
   #create labels tab1
   customer_name_label2 = Label(tab3, text = 'Customer Name: ')
   customer_name_label2.grid(row =0, column = 0)
   
-  phone_number_label2 = Label(tab3, text = 'Phone Number: ')
-  phone_number_label2.grid(row =1, column = 0)
+  customer_id_label2 = Label(tab3, text = 'Customer ID: ')
+  customer_id_label2.grid(row =1, column = 0)
   
   #list customer button 
   input_qry_btn = Button(tab3, text = 'List Customers', command = input_query2)
--- listVehicles.py
+++ listVehicles.py
@@ -9,16 +9,19 @@ from tabs import *
 
 def listVehicles():
   #list query for vehicles
+  Empty = False;
   def input_query():
     
     iq_conn = sqlite3.connect('rental.db')
   
     iq_cur = iq_conn.cursor()
-    if(vehicle_id2.get() == "" and description1.get() == "" and year2.get()  == "" and type2.get() == "" and category2.get() == ""):
-      iq_cur.execute("SELECT * FROM VEHICLE")
+    if(vehicle_id2.get() == "" and description1.get() == "" ):
+      Empty = True;
+      iq_cur.execute("select VIN, Vehicle, printf('$%0.2f', OrderAmount / TotalDays) as [DAILY] from vRentalInfo")
     else:
-      iq_cur.execute("SELECT VehicleID, Description, Year, Type, Category FROM VEHICLE WHERE VehicleID = ? OR Description = ? OR Year = ? OR Type = ? OR Category = ?",
-                    (vehicle_id2.get(), description1.get(), year2.get(), type2.get(), category2.get(),))
+      Empty = False;
+      iq_cur.execute("select VIN, Vehicle, printf('$%0.2f', OrderAmount / TotalDays) as [DAILY] from vRentalInfo WHERE VIN = ? OR VEHICLE = ?",
+                    (vehicle_id2.get(), description1.get(),))
     
     #executes search query when list vehicles button is clicked 
     output_records2 = iq_cur.fetchall()
@@ -27,7 +30,10 @@ def listVehicles():
     
   #print records found
     for output_record in output_records2:
-  	  print_record += str(str(output_record[0])+ " " + output_record[1]+ " " + str(output_record[2])+ " " + str(output_record[3])+ " " + str(output_record[4])+"\n")
+      if Empty:
+  	    print_record += str(str(output_record[0])+ " " + output_record[1]+ " " + str(output_record[2])+ " " + "\n")
+      else:
+        print_record += str(str(output_record[0])+ " " + output_record[1]+ str(output_record[2])+ " " + "\n")
   
     iq_label = Label(tab4, text = print_record)
   
@@ -49,14 +55,6 @@ def listVehicles():
   description1 = Entry(tab4, width = 30)
   description1.grid(row = 1, column = 1)
   
-  year2= Entry(tab4, width = 30)
-  year2.grid(row = 2, column = 1)
-  
-  type2 = Entry(tab4, width = 30)
-  type2.grid(row = 3, column = 1)
-  
-  category2 = Entry(tab4, width = 30)
-  category2.grid(row = 4, column = 1)
   
   #create labels
   vehicle_id_label2 = Label(tab4, text = 'VIN: ')
@@ -65,14 +63,7 @@ def listVehicles():
   description_label1 = Label(tab4, text = 'Description: ')
   description_label1.grid(row =1, column = 0)
   
-  year_label2 = Label(tab4, text = 'Year: ')
-  year_label2.grid(row =2, column = 0)
   
-  type_label2 = Label(tab4, text = 'Type: ')
-  type_label2.grid(row =3, column = 0)
-  
-  category_label2 = Label(tab4, text = 'Category: ')
-  category_label2.grid(row =4, column = 0)
   
   
   input_qry_btn = Button(tab4, text = 'List Vehicles', command = input_query)
